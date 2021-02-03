b0b5afa4874c6f8b9c005d8340a87b4ffdf53777
Admin <rpopov@jinr.ru>
2021-02-03 12:47:29 +0300

    Поправка опо ОС

--- chopper.py
+++ chopper.py
@@ -1,13 +1,42 @@
+import sys
+import glob
+import serial
 import serial.tools.list_ports as port_list
+
+
+def serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
 
 
 def sort_port():
     com_list = []
-    ports = list(port_list.comports())
+    ports = list(serial.tools.list_ports.port_list.comports())
     for p in ports:
         if "MOXA" in str(p):
-            com_list.append(p)
+            com_list.append(str(p))
     return com_list
-
-s = sort_port()
-print(s)
