8cbe266ac7b7695e0164f797ca375a921b89fd29
Admin <rpopov@jinr.ru>
2021-02-03 12:52:27 +0300

    добавил requirements.txt

--- chopper.py
+++ chopper.py
@@ -35,8 +35,10 @@ def serial_ports():
 
 def sort_port():
     com_list = []
-    ports = list(serial.tools.list_ports.port_list.comports())
+    ports = list(serial.tools.list_ports.comports())
     for p in ports:
         if "MOXA" in str(p):
             com_list.append(str(p))
     return com_list
+
+print(serial_ports())
