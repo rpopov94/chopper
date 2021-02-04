083f33edbc0c312b5a40ceef41a6da20968962b7
Admin <rpopov@jinr.ru>
2021-02-04 11:46:37 +0300

    ведем базу данных по каждому порту

--- chopper.py
+++ chopper.py
@@ -1,5 +1,21 @@
-from serial.tools import list_ports
-import time
+from serial.tools import list_ports, miniterm
+import sqlite3
+
+bd = sqlite3.connect("chopper.sqllite")
+
+cur = bd.cursor()
+
+
+def create_table(columm):
+    cur.execute('''
+                create table if not exists chopper(
+                id INT PRIMARY KEY
+                );
+
+                ''')
+    for col in columm:
+        cur.execute("alter table chopper add column '%s' 'string'" % col)
+    bd.commit()
 
 
 def serial_ports():
@@ -7,8 +23,9 @@ def serial_ports():
     ports = list(list_ports.comports())
     for p in ports:
         if "MOXA" in str(p):
-            com_list.append(str(p))
+            com_list.append(str(p)[:4])
     return com_list
 
-while True:
-    time.sleep()
+
+columm = serial_ports()
+create_table(columm)
