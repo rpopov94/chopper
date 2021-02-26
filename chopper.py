86a3d2c7c65d23e9519fcae742f3c3460ccaf416
Admin <rpopov@jinr.ru>
2021-02-26 16:17:01 +0300

    завершил обработку статусного слова

--- chopper.py
+++ chopper.py
@@ -1,42 +1,99 @@
-from serial.tools import list_ports, miniterm
-import sqlite3
-
-
-bd = sqlite3.connect("chopper.sqllite")
-
-cur = bd.cursor()
-
-
-def create_table(columm):
-    cur.execute('''
-                create table if not exists chopper(
-                id INT PRIMARY KEY
-                );
-
-                ''')
-    for col in columm:
-        cur.execute("alter table chopper add column '%s' 'string'" % col)
-    bd.commit()
+from serial import *
+from serial.tools import list_ports
 
 
 def serial_ports():
+    """
+    данные о подключенных портах
+    """
     com_list = []
     ports = list(list_ports.comports())
     for p in ports:
         if "MOXA" in str(p):
             com_list.append(str(p)[:4])
-    return com_list
+    print("Подлюченные устройства:")
+    print(*com_list, sep="\n")
+    if len(com_list) == 0:
+        return 'Нет подключенных устройств'
+    else:
+        return com_list
 
 
-n_ports = serial_ports()
-N = len(n_ports)
+def my_connect(name):
+    '''
+    :param name: имя порта
+    :return: возвращает статус подкючения
+    '''
+    name = name.upper()
+    global ser
+    ser = Serial(
+        port=name,
+        baudrate=9600,
+        bytesize=8,
+        parity='N',
+        stopbits=2,
+        timeout=1,
+        writeTimeout=1
+    )
+    try:
+        ser.isOpen()
+        return 'Succsesful!'
+    except Exception as e:
+        return f'Serial port {ser.name} {e}'
 
 
-def add_to_database(*args, ):
-    cur.execute(f"""INSERT INTO users({[i for i in n_ports]}) 
-                    VALUES('00001', 'Alex', 'Smith', 'male');""")
-    bd.commit()
+def generate_command(nblock, register, cmd, data):
+    """
+    :param nblock: номер блока
+    :param register: номер регистра
+    :param cmd: команда чтения/запись
+    :param data: слово состояния
+    """
+    mode = ''
+    STX = '%c' % 2
+    HSC = '%c' % 0x7c
+    bl = '%02x' % nblock
+    Reg = '%03x' % register
+    if cmd.upper() == 'R':
+        mode = '%c' % 0x52
+    elif cmd.upper() == 'W':
+        mode = '%c' % 0x57
+    ENQ = '%c' % 5
+    DATA = '%04x' % data
+    ETX = '%c' % 3
+    request = f'{STX}{HSC}{bl}{HSC}{Reg}{HSC}{mode}{HSC}{DATA}{HSC}{ENQ}{HSC}{ETX}'.encode('utf-8')
+    print('Request is:  ', request)
+    ser.write(request)
 
 
-speeds = ['1200', '2400', '4800', '9600', '19200', '38400', '57600', '115200']
+def myresponse():
+    """
+    :return: ответ блока на запрос
+    """
+    response = ser.readline()
+    ser.close()
+    print('Response is: ', response)
+    return response
 
+
+def get_command(string):
+    """
+    :param string: команда блоку
+    :return: возвращает ответ
+    """
+    myrequest = string.split(',')
+    nblock, register, cmd, data = myrequest
+    generate_command(int(nblock), int(register), cmd, int(data))
+    reply = myresponse().decode('utf-8')[2:-4]
+    reply = reply.split('|')
+    return f'reply = |{int(reply[0], 16)}|' \
+           f'{int(reply[1], 16)}|' \
+           f'{reply[2]}|' \
+           f'{int(reply[3], 16)}|'
+
+
+def smart_function(string):
+    if "COM" in string.upper():
+        return my_connect(string)
+    elif ',' in string:
+        return get_command(string)
