ab8d522ad3a8a8ebb3f15383375bb3226a5b8583
rpopov94 <52166414+rpopov94@users.noreply.github.com>
2021-04-03 21:21:02 +0300

    добавил комментарии, как нужно править

--- app/main.py
+++ app/main.py
@@ -8,8 +8,8 @@ Form, Base = uic.loadUiType("dialog.ui")
 
 
 class UpdateInterface(QThread):
-    req = b''
-
+    req = smc32_command(int(value), 0, 'r', 0).decode('utf-8')
+    
     def __init__(self):
         QThread.__init__(self)
         self.test = config()
@@ -30,6 +30,7 @@ class MyWindow(QtWidgets.QWidget, Form):
         self.setupUi(self)
         self.current = UpdateInterface()
         self.current.start()
+        self.SetData()
         self.timer = QtCore.QTimer()
         self.timer.timeout.connect(self.SetData)
         self.timer.start(1000)
@@ -39,6 +40,63 @@ class MyWindow(QtWidgets.QWidget, Form):
         self.pushButton_11.clicked.connect(self.on_disconnect)
         self.pushButton_12.clicked.connect(self.on_response)
         self.comboBox.addItems(serial_ports())
+        
+    def SetData(self):
+        '''Стереть предыдущее состояние'''
+        self.label.setStyleSheet("background-color: #DCDCDC")
+        self.label_29.setStyleSheet("background-color: #DCDCDC")
+        self.label_45.setStyleSheet("background-color: #DCDCDC")
+        self.label_46.setStyleSheet("background-color: #DCDCDC")
+        self.label_47.setStyleSheet("background-color: #DCDCDC")
+        self.label_48.setStyleSheet("background-color: #DCDCDC")
+        self.label_49.setStyleSheet("background-color: #DCDCDC")
+        self.label_50.setStyleSheet("background-color: #DCDCDC")
+        self.label_51.setStyleSheet("background-color: #DCDCDC")
+        self.label_52.setStyleSheet("background-color: #DCDCDC")
+        self.label_53.setStyleSheet("background-color: #DCDCDC")
+        self.label_54.setStyleSheet("background-color: #DCDCDC")
+        self.label_55.setStyleSheet("background-color: #DCDCDC")
+        self.label_56.setStyleSheet("background-color: #DCDCDC")
+        self.label_57.setStyleSheet("background-color: #DCDCDC")
+        self.label_58.setStyleSheet("background-color: #DCDCDC")
+        '''
+        вставить кусок, который будет возврашать словарь состояния
+        и менять стиль конкретной строчки
+        '''
+        response = 'сюда вставить функцию ответа'
+        if response[0] == '1':
+            self.label_59.setStyleSheet("background-color: red")
+        if response[1] == '1':
+            self.label_30.setStyleSheet("background-color: red")
+        if response[2] == '1':
+            self.label_45.setStyleSheet("background-color: red")
+        if response[3] == '1':
+            self.label_46.setStyleSheet("background-color: red")
+        if response[4] == '1':
+            self.label_47.setStyleSheet("background-color: red")
+        if response[5] == '1':
+            self.label_48.setStyleSheet("background-color: red")
+        if response[6] == '1':
+            self.label_49.setStyleSheet("background-color: red")
+        if response[7] == '1':
+            self.label_50.setStyleSheet("background-color: red")
+        if response[8] == '1':
+            self.label_51.setStyleSheet("background-color: red")
+        if response[9] == '1':
+            self.label_52.setStyleSheet("background-color: red")
+        if response[10] == '1':
+            self.label_53.setStyleSheet("background-color: red")
+        if response[11] == '1':
+            self.label_54.setStyleSheet("background-color: red")
+        if response[12] == '1':
+            self.label_55.setStyleSheet("background-color: red")
+        if response[13] == '1':
+            self.label_56.setStyleSheet("background-color: red")
+        if response[14] == '1':
+            self.label_57.setStyleSheet("background-color: red")
+        if response[15] == '1':
+            self.label_58.setStyleSheet("background-color: red")
+        
 
     def ch_update_online(self):
         response = online_parameters(int(self.plainTextEdit_3.toPlainText()))
